9b96431cef88a824a774de07e6a89915fc94dc26
yadavsuraj999 <yadavsuraj8425@gmail.com>
2025-05-29 16:20:03 +0530

    add buttons

--- pr-3/src/components/TodoList.jsx
+++ pr-3/src/components/TodoList.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const TodoList = () => {
     const [addtask, setAddtask] = useState("");
     const [tasks, setTasks] = useState([]);
+    const [fillter, setFillter] = useState("all");
+    const [fillterarr, setFillterarr] = useState([]);
+
 
     const handleChange = (e) => {
         setAddtask(e.target.value);
@@ -21,16 +24,34 @@ const TodoList = () => {
         setAddtask("");
     };
 
-    const tackComplete = (id) => {
+    const taskComplete = (id) => {
         const updatedTasks = tasks.map((task) =>
-            task.id === id ? { ...task, isComplete: !task.isComplete } : task
+            task.id === id ? { ...task, isComplete: true } : task
         );
         setTasks(updatedTasks);
     };
 
+    useEffect(() => {
+        let temparr = []
+        if (fillter === "all") {
+            temparr = tasks
+        } else if (fillter === "pending") {
+            temparr = tasks.filter((task) => {
+                return task.isComplete == false;
+            })
+        } else {
+            temparr = tasks.filter((task) => {
+                return task.isComplete == true;
+            })
+        }
+        setFillterarr(temparr)
+    }, [tasks, fillter])
+
+
+
     return (
         <div className="container mx-auto">
-            <div className="text-center">
+            <div className="text-center mt-24">
                 <div className="inline-block border-2 p-6 rounded-lg">
                     <h1 className="font-bold text-4xl my-5">✅ Todo List</h1>
                     <div className="flex gap-2 justify-center pb-4">
@@ -43,54 +64,60 @@ const TodoList = () => {
                         />
                         <button
                             type="button"
-                            className="bg-red-950 text-white rounded-lg p-2"
+                            className="bg-green-600 text-white rounded-lg p-2"
                             onClick={handleClick}
                         >
                             Add Task
                         </button>
                     </div>
-                </div>
-
-                <div className="mt-6 relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th className="p-4">#</th>
-                                <th className="px-6 py-3">Task</th>
-                                <th className="px-6 py-3">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {tasks.map((taskItem) => (
-                                <tr
-                                    key={taskItem.id}
-                                    className="bg-white border-b hover:bg-gray-50"
-                                >
-                                    <td className="w-4 p-4">
-                                        <input
-                                            type="checkbox"
-                                            checked={taskItem.isComplete}
-                                            onChange={() => taskComplete(taskItem.id)}
-                                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500"
-                                        />
-                                    </td>
-                                    <td className="px-6 py-4 font-medium text-gray-900">
-                                        {taskItem.task}
-                                    </td>
-                                    <td className="px-6 py-4 text-black font-semibold">
-                                        {taskItem.isComplete ? "✅ Completed" : "❌ Incomplete"}
-                                    </td>
-                                </tr>
-                            ))}
-                            {tasks.length === 0 && (
+                    <div className="flex justify-center gap-3">
+                        <button className="border px-3 py-2 text-center rounded-2xl focus:bg-red-950 focus:text-white" onClick={() => {
+                            setFillter("all")
+                        }} >All</button>
+                        <button className="border px-3 py-2 text-center rounded-3xl focus:bg-red-950 focus:text-white" onClick={() => {
+                            setFillter("pending")
+                        }}>Pending</button>
+                        <button className="border px-3 py-2 text-center rounded-3xl focus:bg-red-950 focus:text-white" onClick={() => {
+                            setFillter("complete")
+                        }}>Complete</button>
+                    </div>
+                    <div className="mt-6 relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <td colSpan="3" className="text-center py-4 text-gray-500">
-                                        No tasks added yet.
-                                    </td>
+                                    <th className="p-4"></th>
+                                    <th className="px-6 py-3">Task</th>
+                                    <th className="px-6 py-3">Status</th>
                                 </tr>
-                            )}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {tasks.length === 0 
+                                    ?(<tr><td colSpan="3" className="text-center py-4 text-gray-500">No tasks added yet.</td></tr>)
+                                    :fillterarr.map((taskItem) => (
+                                        <tr
+                                            key={taskItem.id}
+                                            className="bg-white border-b hover:bg-gray-50"
+                                        >
+                                            <td className="w-4 p-4">
+                                                <input
+                                                    type="checkbox"
+                                                    checked={taskItem.isComplete}
+                                                    onChange={() => taskComplete(taskItem.id)}
+                                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500"
+                                                />
+                                            </td>
+                                            <td className="px-6 py-4 font-medium text-gray-900">
+                                                {taskItem.task}
+                                            </td>
+                                            <td className="px-6 py-4 text-black font-semibold">
+                                                {taskItem.isComplete ? "✅ Completed" : "❌ Incomplete"}
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
