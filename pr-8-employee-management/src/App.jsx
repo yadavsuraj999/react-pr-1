32d7acf759b04d83996b349da30e55ac8cc6d9cc
yadavsuraj999 <yadavsuraj8425@gmail.com>
2025-06-20 12:49:18 +0530

    day-1

--- pr-8-employee-management/src/App.jsx
+++ pr-8-employee-management/src/App.jsx
@@ -1,20 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
+import { ToastContainer } from 'react-toastify';
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Login from "./pages/Loin"
+import { useEffect, useState } from "react";
+import Employee from "./pages/Employee";
 
 const App = () => {
+  const [isloggedin, setIsLoggedin] = useState(false)
+
+  useEffect(() => {
+    let loggedinfo = JSON.parse(localStorage.getItem("isloggedin")) || false
+    setIsLoggedin(loggedinfo)
+  }, [])
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login setIsLoggedin={setIsLoggedin} />} />
+        <Route path="/employee" element={<Employee />} />
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
   )
 }
--- pr-8-employee-management/src/pages/Loin.jsx
+++ pr-8-employee-management/src/pages/Loin.jsx
@@ -1,42 +1,66 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const[Input, useInput] = useStateState()
 
-const handlechange = (e)=>{
+const Login = ({ setIsLoggedin }) => {
+    const [input, setInput] = useState({
+        email: "",
+        password: ""
+    })
 
-}
+    const navigate = useNavigate()
 
-const Login = () => {
+    const handlechange = (e) => {
+        setInput({ ...input, [e.target.id]: e.target.value })
+    }
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+
+        if (input.email == "admin@gmail.com" && input.password == "admin@123") {
+            toast.success("Logged In SuccesFully");
+            setIsLoggedin(true)
+            localStorage.setItem("isloggedin", JSON.parse(true))
+            navigate("/employee")
+        } else {
+            toast.error("user or password was worng")
+        }
+        setInput({
+            email: "",
+            password: ""
+        })
+    }
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             <div className="bg-white p-10 rounded-2xl shadow-xl w-full max-w-sm">
                 <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
                     Admin Panel Login
                 </h2>
-                <form className="space-y-5">
+                <form className="space-y-5" onSubmit={handlesubmit}>
                     <div>
-                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
                             Username
                         </label>
                         <input
-                            type="text"
+                            type="text" id="email" value={input.email}
                             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                             placeholder="admin@example.com" onChange={handlechange}
                         />
                     </div>
                     <div>
-                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
                             Password
                         </label>
                         <input
-                            type="password"
+                            type="password" id="password" value={input.password}
                             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="••••••••"
-
+                            placeholder="••••••••" onChange={handlechange}
                         />
                     </div>
                     <button
-                        type="button"
+                        type="submit"
                         className="w-full bg-blue-500 text-white py-2 rounded-lg font-semibold "
                     >
                         Login
